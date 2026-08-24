8df148dc00b4ca2d72a5b9140e580dccadee3522
Qiuchi-Hu <74480846+Qiuchi-Hu@users.noreply.github.com>
2026-08-24 13:11:11 +0800

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        moves = [[0,1],[0,-1],[1,0],[-1,0]]
+        rows = len(grid)
+        cols = len(grid[0])
+
+        groups = 0
+
+        def dfs(r,c):
+            nonlocal moves,grid
+
+            grid[r][c] = "0"
+            for r_move, c_move in moves:
+                r_new = r + r_move
+                if r_new >=0 and r_new < rows:
+                    c_new = c + c_move
+                    if c_new >=0 and c_new < cols and grid[r_new][c_new]=="1":
+                        dfs(r_new,c_new)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    groups+=1
+                    dfs(r,c)
+
+        
+        return groups
