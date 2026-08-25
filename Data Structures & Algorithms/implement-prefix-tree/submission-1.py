495a914ae144fcf2d1c26acb806b28c69c2023f4
Qiuchi-Hu <74480846+Qiuchi-Hu@users.noreply.github.com>
2026-08-25 17:41:48 +0800

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,50 @@
+class TreeNode:
+    def __init__(self):
+        self.child = {}
+        self.is_end = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.child = {}
+        
+    def insert(self, word: str) -> None:
+        trace = self.child
+        len_word = len(word)
+        for i in range(len_word):
+            current = word[i]
+            if not current in trace:
+                trace[current] = TreeNode()
+            trace = trace[current]
+            if i == len_word-1:
+                trace.is_end = True
+                continue
+            trace = trace.child
+
+    def search(self, word: str) -> bool:
+        len_word = len(word)
+        trace = self.child
+        current = None
+        for i in range(len_word-1):
+            current = word[i]
+            if current not in trace:
+                return False
+            trace = trace[current].child
+        current = word[len_word-1]
+        if current not in trace or not trace[current].is_end:
+            return False
+        return True
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        len_word = len(prefix)
+        trace = self.child
+        for i in range(len_word):
+            current = prefix[i]
+            if current not in trace:
+                return False
+            trace = trace[current].child
+        
+        return True
+        
+        
