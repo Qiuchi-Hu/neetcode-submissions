105df2aee26d30c5b65905957b0d8690a9fdbead
Qiuchi-Hu <74480846+Qiuchi-Hu@users.noreply.github.com>
2026-09-05 16:06:18 +0800

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        len_nums = len(nums)
+        if len_nums == 1:
+            return 0
+        count = 0
+        left = 0
+        right = 1
+        max_right = 2
+
+        while right < len_nums:
+            for i in range(left, right):
+                if nums[i]+i>=len_nums-1:
+                    return count+1
+                max_right = max(max_right,nums[i]+i+1)
+            count+=1
+            left = right
+            right = max_right
+                
+
+                
+                
+            
